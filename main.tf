d73f3a5b2db224c36865e3055af1a63523926883
marcus442 <134263884+marcus442@users.noreply.github.com>
2025-03-14 11:22:14 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,31 +3,31 @@ provider "proxmox" {
   username = "root@pam"
   password = "Par240XXX"
   insecure = true
- }
 
   ssh {
     agent = true
-    # Uncomment below for using api_token
-    # username = "root"
+    
   }
+ }
+
 
 resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
   content_type = "iso"
   datastore_id = "local"
-  node_name    = "pve"
+  node_name    = var.proxmox_host
 
   url = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
 }
 
-
 resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
-  name      = "test-ubuntu"
-  node_name = "pve"
+  count = 3
+  name  = "ubuntu-vm-${count.index + 1}"
+  node_name = var.proxmox_host
 
   initialization {
     ip_config {
       ipv4 {
-        address = "192.168.111.191/24"
+        address = "192.168.111.${192 + count.index}/24"
         gateway = "192.168.111.1"
       }
     }
@@ -41,6 +41,7 @@ resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
   disk {
     datastore_id = "local-lvm"
     file_id      = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
+    #file_id     = "local:iso/ubuntu-cloudimg-jammy.img" 
     interface    = "virtio0"
     iothread     = true
     discard      = "on"
