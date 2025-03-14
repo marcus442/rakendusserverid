0a37d718581031bbb072241dd613b331bb673f4a
marcus442 <134263884+marcus442@users.noreply.github.com>
2025-03-14 11:22:29 +0200

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -2,7 +2,7 @@ variable "ssh_key" {
   default = "ssh-rsa AAAAC3NzaC1lZDI1NTE5AAAAIHxGfpIVOiL6WoRifROUqcu2UKcxMlyt52lxw4KWeOV6 marcus@A326-15"
 }
 variable "proxmox_host" {
-  default = "proxmox1"
+  default = "labor"
 }
 variable "template_name" {
   default = "8001"
